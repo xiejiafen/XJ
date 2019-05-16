382ad96690385d2b42ddf4237aff4bb312b84b9d
cxyxc <1730084276@qq.com>
2019-05-16 22:20:16 +0800

    调整代码 1.文件名添加后缀 2.自动生成产出目录

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import werkzeug, os
 import traceback
+import uuid
 
 app = Flask(__name__, 
             static_url_path='/static', 
@@ -24,17 +25,28 @@ class ImageUpload(Resource):
     def post(self):
         upload_files = request.files.getlist('file')
         result_files = []
+
+        # 创建目录 
+        if not os.path.exists(IMG_FOLDER):
+            os.mkdir(IMG_FOLDER)
+
         try:
             fo = open(PLUGIN_CONFIG_FILE, "w")
             for file in upload_files:
-                # 计算输出结果文件名
+
                 filepath_arr = file.filename.split('.')
+                # filename 需要应使用 secure_filename 但处理后会丢失 _ 前缀
+                filepath_arr[-2] = filepath_arr[-2]  + '_' + str(uuid.uuid1())
+
+                filename = '.'.join(filepath_arr)
+                
+                # 计算输出结果文件名
                 filepath_arr[-2] += '_result'
                 result_files.append( '/' + IMG_FOLDER + '/' + '.'.join(filepath_arr) )
 
-                filename = file.filename # filename 需要应使用 secure_filename 但处理后会丢失 _ 前缀
                 # 计算绝对路径存放图片
                 upload_path = os.path.join(DIR_NAME, IMG_FOLDER, filename)
+
                 file.save(upload_path)
 
                 # 写入 path.txt
